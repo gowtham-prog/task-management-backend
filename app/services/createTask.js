f39f60b50ce07ea6558aa3e5024b5346e3f23bd6
gowtham-prog <gowthamkumarvulluri@gmail.com>
2024-11-05 20:29:33 +0530

    altered createTask

--- app/services/createTask.js
+++ app/services/createTask.js
@@ -3,11 +3,18 @@ const createtask_logic = require("../accessors/createTask_logic");
 const createTask = async (req, res) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).send({ message: "Authorization token is missing or invalid" });
+    }
+
     const data = await createtask_logic(token, req.body);
-    res.status(500).send(data);
+
+    res.status(201).send(data);
   } catch (err) {
-    res.status(400).send({ message: err.message });
+    console.error("Error creating task:", err);
+    res.status(500).send({ message: err.message || "Internal server error" });
   }
 };
+
 
 module.exports = createTask;
